fbbc9701e3f9fee23a3913da388fff21ded25bc7
newapok69 <126722647+newapok69@users.noreply.github.com>
2023-03-07 12:19:28 -0600

    Update BattleGroundJoinAction.cpp
    Implicit conversion for NULL to unsigned int.

--- playerbot/strategy/actions/BattleGroundJoinAction.cpp
+++ playerbot/strategy/actions/BattleGroundJoinAction.cpp
@@ -969,9 +969,9 @@ bool BGLeaveAction::Execute(Event& event)
         ai->SetMaster(NULL);
 
     ai->ResetStrategies();
-    ai->GetAiObjectContext()->GetValue<uint32>("bg type")->Set(NULL);
-    ai->GetAiObjectContext()->GetValue<uint32>("bg role")->Set(NULL);
-    ai->GetAiObjectContext()->GetValue<uint32>("arena type")->Set(NULL);
+    ai->GetAiObjectContext()->GetValue<uint32>("bg type")->Set(0);
+    ai->GetAiObjectContext()->GetValue<uint32>("bg role")->Set(0);
+    ai->GetAiObjectContext()->GetValue<uint32>("arena type")->Set(0);
     return true;
 }
 
@@ -1215,9 +1215,9 @@ bool BGStatusAction::Execute(Event& event)
 #endif
         bot->GetSession()->HandleLeaveBattlefieldOpcode(packet);
         ai->ResetStrategies();
-        ai->GetAiObjectContext()->GetValue<uint32>("bg type")->Set(NULL);
-        ai->GetAiObjectContext()->GetValue<uint32>("bg role")->Set(NULL);
-        ai->GetAiObjectContext()->GetValue<uint32>("arena type")->Set(NULL);
+        ai->GetAiObjectContext()->GetValue<uint32>("bg type")->Set(0);
+        ai->GetAiObjectContext()->GetValue<uint32>("bg role")->Set(0);
+        ai->GetAiObjectContext()->GetValue<uint32>("arena type")->Set(0);
         ai::PositionMap& posMap = context->GetValue<ai::PositionMap&>("position")->Get();
         ai::PositionEntry pos = context->GetValue<ai::PositionMap&>("position")->Get()["bg objective"];
         pos.Reset();
@@ -1316,9 +1316,9 @@ bool BGStatusAction::Execute(Event& event)
             bot->GetSession()->HandleBattlefieldPortOpcode(packet);
 #endif
             ai->ResetStrategies();
-            ai->GetAiObjectContext()->GetValue<uint32>("bg type")->Set(NULL);
-            ai->GetAiObjectContext()->GetValue<uint32>("bg role")->Set(NULL);
-            ai->GetAiObjectContext()->GetValue<uint32>("arena type")->Set(NULL);
+            ai->GetAiObjectContext()->GetValue<uint32>("bg type")->Set(0);
+            ai->GetAiObjectContext()->GetValue<uint32>("bg role")->Set(0);
+            ai->GetAiObjectContext()->GetValue<uint32>("arena type")->Set(0);
             sRandomPlayerbotMgr.BgBots[queueTypeId][bracketId][TeamId]--;
             return true;
         }
